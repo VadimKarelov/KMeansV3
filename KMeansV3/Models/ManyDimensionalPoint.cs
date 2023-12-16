7d4cfd383d59bebef458d6ccaf1732634ebc08ed
VadimKarelov <vadim.karelov.02@mail.ru>
2023-12-16 18:34:54 +0500

    Сделал основу

---
+++ KMeansV3/Models/ManyDimensionalPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace KMeansV3.Models;
+
+public partial class ManyDimensionalPoint : INotifyPropertyChanged
+{
+    public string Name { get; set; }
+    
+    public double[] Values { get; set; }
+
+    public ManyDimensionalPoint Clone(ManyDimensionalPoint point)
+    {
+        return new ManyDimensionalPoint()
+        {
+            Values = point.Values
+        };
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}:({string.Join(',', Values)})";
+    }
+}
--- KMeansV3/ViewModels/MainWindowViewModel.cs
+++ KMeansV3/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,86 @@
-﻿namespace KMeansV3.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using KMeansV3.Models;
+using Newtonsoft.Json;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace KMeansV3.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    [Reactive]
+    [Range(1,10,ErrorMessage = "от 1 до 10")]
+    public int NumberOfCenters { get; set; }
+    
+    [Reactive]
+    public ObservableCollection<ManyDimensionalPoint> Points { get; set; }
+    
+    
+    public ReactiveCommand<Unit, Unit> ComputeKMeansCommand { get; set; }
+    
+    public ReactiveCommand<Unit, Unit> ComputeCosinusCommand { get; set; }
+    
+
+    public MainWindowViewModel()
+    {
+        Points = new(ReadPointsOrCreateDefault());
+
+        var isCommandsAvailable = this.WhenAny(vm => vm.NumberOfCenters,
+            n => n.Value >= 1 && n.Value <= 10);
+    }
+
+    public void KMeans()
+    {
+        
+    }
+    
+    private List<ManyDimensionalPoint> ReadPointsOrCreateDefault()
+    {
+        string path = "input.json";
+
+        try
+        {
+            using var reader = new StreamReader(path);
+            var inputData = JsonConvert.DeserializeObject<ManyDimensionalPoint[]>(reader.ReadToEnd());
+
+            if (inputData == null || !inputData.Any())
+            {
+                throw new ArgumentNullException(nameof(inputData));
+            }
+
+            return inputData.ToList();
+        }
+        catch
+        {
+            Random random = new Random();
+
+            const string pointsLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const int dimensions = 5;
+
+            var res = new List<ManyDimensionalPoint>();
+
+            for (int i = 0; i < pointsLetters.Length; i++)
+            {
+                List<double> values = new();
+
+                for (int j = 0; j < dimensions; j++)
+                {
+                    values.Add(random.Next(-5, 50));
+                }
+
+                res.Add(new ManyDimensionalPoint() { Name = $"Точка {pointsLetters[i]}", Values = values.ToArray() });
+            }
+
+            using StreamWriter writer = new StreamWriter(path);
+            writer.Write(JsonConvert.SerializeObject(res.ToArray()));
+
+            return res;
+        }
+    }
 }
